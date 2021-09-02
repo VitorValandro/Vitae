0f759a3087388ab096c7dc0e234aff0f1bfa63fd
Vitor Valandro <vitormateusd@gmail.com>
2021-09-02 10:01:48 -0300

    Add phone and password arguments to User model

--- server/models/User.py
+++ server/models/User.py
@@ -1,18 +1,29 @@
-from database import db
-from dataclasses import dataclass
+from flask_marshmallow import Marshmallow
 
-@dataclass
+from database import db
+
+ma = Marshmallow()
+
 class User(db.Model):
-  def __init__(self, username, email):
+  def __init__(self, username, email, phone, password):
     self.username = username
     self.email = email
-  
-  # Definição de tipos pro parser @dataclass
-  id: int
-  username: str
-  email: str
+    self.phone = phone
+    self.password = password
 
   # Definição das colunas
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
+  phone = db.Column(db.String(18), nullable=True)
+  password = db.Column(db.String(128), nullable=False)
+
+  # Marshmallow schema para converter o objeto User em JSON
+class UserSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'username', 'email', 'phone', 'password')
+
+# Esquema para usuário individual
+user_schema = UserSchema() 
+# Esquema para retorno de múltiplos usuários (-> array)
+users_schema = UserSchema(many=True)
--- server/resources/User.py
+++ server/resources/User.py
@@ -1,26 +1,47 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
+from flask.json import JSONDecoder
 from flask_restful import Resource, reqparse
+import bcrypt
 
 from database import db
-from models.User import User
+from models.User import User, user_schema, users_schema
 
 parser = reqparse.RequestParser()
 parser.add_argument('username', type=str)
 parser.add_argument('email', type=str)
+parser.add_argument('phone', type=str)
+parser.add_argument('password', type=str)
 
 class UserRoute(Resource):
-  def get(self):
-    response = User.query.all()
-    return jsonify(response)
+  def get(self):  
+    response = User.query.with_entities(User.username).all()
+    JSONresponse = users_schema.dump(response)
+    return make_response(jsonify(JSONresponse), 201)
   
   def post(self):
     request.get_json()
     args = parser.parse_args()
     username = str(args['username'])
     email = str(args["email"])
+    phone = str(args["phone"])
+    plain_password = str(args["password"]).encode("utf-8")
+    
+    if username is None or plain_password is None or email is None:
+      return make_response(jsonify({"error":"Todas as informações devem ser preenchidas"}), 400)
 
-    new_user = User(username, email)
-    db.session.add(new_user)
-    db.session.commit()
+    if User.query.filter_by(username = username).first() is not None:
+      return make_response(jsonify({"error":"Já existe um usuário com esse nome"}), 400)
+    
+    if User.query.filter_by(email = email).first() is not None:
+      return make_response(jsonify({"error":"Já existe um usuário com esse email"}), 400)
 
-    return jsonify(username=username, email=email)
+    password = str(bcrypt.hashpw(plain_password, bcrypt.gensalt()))
+    new_user = User(username, email, phone, password)
+
+    try:
+      db.session.add(new_user)
+      db.session.commit()
+      JSONresponse = user_schema.dump(new_user)
+      return make_response(jsonify(JSONresponse), 201)
+    except:
+      return make_response(jsonify({"error":"Não foi possível registrar o usuário. Tente novamente mais tarde."}), 500)
