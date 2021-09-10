e2a538e62ed4ed353db2eb4297dece741e0a1aa1
Vitor Valandro <vitormateusd@gmail.com>
2021-09-10 15:51:01 -0300

    Abstract Home HTML into UserCard component

--- web/src/pages/Home/Home.js
+++ web/src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './Home.css'
 import TopBar from '../../components/TopBar/TopBar';
 import Footer from '../../components/Footer/Footer';
+import UserCard from '../../components/UserCard/UserCard';
 
 function Home() {
   return (
@@ -10,94 +11,7 @@ function Home() {
       <TopBar />
       <div className="home-container">
         <div className="home-content">
-          <div className="user-info-container">
-            <div className="user-info-left">
-              <img 
-                className="user-info-image" 
-                src="https://github.com/V.png" 
-                alt="foto do usuário"
-              />
-            </div>
-            <div className="user-info-right">
-              <span className="user-info-title">Vitor Matheus Valandro da Rosa</span>
-              <span className="user-info-subtitle">Professor Doutor Alcione Talaska</span>
-              <span className="user-info-description">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                It has survived not only five centuries, but also the leap into electronic typesetting, 
-                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
-                like Aldus PageMaker including versions of Lorem Ipsum.
-              </span>
-            </div>
-          </div>
-          <div className="user-info-container">
-            <div className="user-info-left">
-              <img
-                className="user-info-image"
-                src="https://github.com/V.png"
-                alt="foto do usuário"
-              />
-            </div>
-            <div className="user-info-right">
-              <span className="user-info-title">Vitor Matheus Valandro da Rosa</span>
-              <span className="user-info-subtitle">Professor Doutor Alcione Talaska</span>
-              <span className="user-info-description">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                It has survived not only five centuries, but also the leap into electronic typesetting,
-                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-                like Aldus PageMaker including versions of Lorem Ipsum.
-              </span>
-            </div>
-          </div>
-          <div className="user-info-container">
-            <div className="user-info-left">
-              <img
-                className="user-info-image"
-                src="https://github.com/V.png"
-                alt="foto do usuário"
-              />
-            </div>
-            <div className="user-info-right">
-              <span className="user-info-title">Vitor Matheus Valandro da Rosa</span>
-              <span className="user-info-subtitle">Professor Doutor Alcione Talaska</span>
-              <span className="user-info-description">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                It has survived not only five centuries, but also the leap into electronic typesetting,
-                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-                like Aldus PageMaker including versions of Lorem Ipsum.
-              </span>
-            </div>
-          </div>
-          <div className="user-info-container">
-            <div className="user-info-left">
-              <img
-                className="user-info-image"
-                src="https://github.com/V.png"
-                alt="foto do usuário"
-              />
-            </div>
-            <div className="user-info-right">
-              <span className="user-info-title">Vitor Matheus Valandro da Rosa</span>
-              <span className="user-info-subtitle">Professor Doutor Alcione Talaska</span>
-              <span className="user-info-description">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                It has survived not only five centuries, but also the leap into electronic typesetting,
-                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-                like Aldus PageMaker including versions of Lorem Ipsum.
-              </span>
-            </div>
-          </div>
+          <UserCard />
         </div>
       </div>
       <Footer />
