48540c1f5c9d55385a3a30a30c28c4a93c0ca88d
Vitor Valandro <vitormateusd@gmail.com>
2021-10-06 07:44:14 -0300

    Add config.py

---
+++ server/config.py
@@ -0,0 +1,7 @@
+# Preencha API_TOKEN com um hash único que servirá de identificador para a API.
+# Preencha CLIENT_ORIGIN com o URL do front end que poderá consumir a API (CORS).
+# Preencha UPLOAD_FOLDER com o URL da pasta onde os arquivos de upload serão armazenados
+
+API_TOKEN = "*F-J@NcRfUjXn2r5u8x/A?D(G+KbPdSgVkYp3s6v9y$B&E)H@McQfThWmZq4t7w!"
+CLIENT_ORIGIN = "http://localhost:3000"
+UPLOAD_FOLDER = "./uploads"
